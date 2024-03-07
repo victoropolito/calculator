34c5bfc09fa9645e00329e902b2801fe4819046b
vtxxxx <victor_polito13@hotmail.com>
2024-03-07 18:45:46 -0300

    first commit - calculator component

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Calculator from './main/Calculator';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <h1>Calculadora</h1>
+    <Calculator />
   </React.StrictMode>
 );
 
