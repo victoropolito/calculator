a8e7d5e999b5e4acfd11fa1ddaa3e79913ca45b1
vtxxxx <victor_polito13@hotmail.com>
2024-03-13 12:16:57 -0300

    initial state and adding digits
    there is also a rule that ensures that there is no more than one dot in the actual number

--- src/main/Calculator.jsx
+++ src/main/Calculator.jsx
@@ -4,7 +4,18 @@ import './Calculator.css'
 import Button from '../components/Button'
 import Display from '../components/Display'
 
+const initialState = {
+  displayValue: '0',
+  clearDisplay: false,
+  operation: null,
+  values: [0, 0], 
+  current: 0
+}
+
 export default class Calculator extends Component {
+
+  state = {...initialState}
+
   constructor(props) {
     super(props)
     this.clearMemory = this.clearMemory.bind(this)
@@ -12,9 +23,8 @@ export default class Calculator extends Component {
     this.addDigit = this.addDigit.bind(this)
   }
 
-
   clearMemory() {
-    console.log('limpar')
+    this.setState({...initialState})
   }
   
   setOperation(operation) {
@@ -22,13 +32,29 @@ export default class Calculator extends Component {
   }
   
   addDigit(n) {
-    console.log(n)
+    if(n === '.' && this.state.displayValue.includes('.')) {
+      return
+    }
+
+    const clearDisplay = this.state.displayValue === '0'
+      || this.state.clearDisplay
+    const currentValue = clearDisplay ? '' : this.state.displayValue
+    const displayValue = currentValue + n
+    this.setState({ displayValue, clearDisplay: false })
+
+    if (n !== '.') {
+      const i = this.state.current
+      const newValue = parseFloat(displayValue)
+      const values = [...this.state.values]
+      values[i] = newValue
+      this.setState({ values })
+    }
   }
 
   render() {
     return (
       <div className="calculator">
-        <Display value={100}/>
+        <Display value={this.state.displayValue}/>
         <Button label="AC" click={this.clearMemory} triple/>
         <Button label="/" click={this.setOperation} operation/>
         <Button label="7" click={this.addDigit}/>
